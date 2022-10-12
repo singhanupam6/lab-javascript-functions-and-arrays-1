63fa7e7888c5efa388a70a93c5550df4846bee50
singhanupam6 <anupamrajawat4@gmail.com>
2022-10-12 21:19:43 +0530

    project25

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,62 @@
 // Progression #1: Greatest of the two numbers
+function greatestOfNumbers(){
+  var num1=prompt("Enter first number");
+  var num2=prompt("Enter second number");
 
+  if(num1>num2)
+  {
+    document.write(num1," is larger");
+  }
+  else if(num2>num1)
+  {
+    document.write(num2," is larger");
+  }
+}
+greatestOfNumbers();
 // Progression #2: The lengthy word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
+function lengthyWord(words)
+{
+  let longestWord=0;
+  for(var i=0;i<words.length;i++){
+    if(words[i].length>longestWord){
+      longestWord=words[i].length
+    }
+  }
+  return longestWord;
+}
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
+function netPrice(numbers)
+{
+  var sum=0;
+  for(var i=0;i<numbers.length;i++)
+  {
+    sum=sum+numbers.length[i];
+  }
+  return sum;
+}
 // Progression #4: Calculate the average
 // Progression 4.1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
+function midOfPoints(numbersAvg)
+{
+  for(var i=0;i<numbersAvg.length;i++)
+  {
+    total=total+numbersAvg[i];
+  }
+  return total/numbersAvg.length;
+}
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
-
+function averageWordLength(wordsArr)
+{
+  for(var i=0;i<wordsArr.length;i++)
+  {
+    total=total+wordsArr[i].length;
+  }
+  return total/wordsArr.length;
+}
 // Progression #5: Unique arrays
 const wordsUnique = [
   'bread',
@@ -28,10 +72,33 @@ const wordsUnique = [
   'egg',
   'flour'
 ];
-
+function uniqueArray(items)
+{
+  for (var i=0;i<items.length;i++)
+  {
+    for(var j=i+1;j<items.length;j++)
+    {
+      if(items[i]==items[j])
+      {
+        items.splice(j,1);
+      }
+    }
+  }
+  return items;
+}
+uniqueArray(items);
 // Progression #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
-
+function searchElement(wordsFind,key)
+{
+  words.forEach(element => {
+    if(element==key)
+    {
+      return true;
+    }
+  });
+  return false;
+}
 // Progression #7: Count repetition
 const wordsCount = [
   'machine',
@@ -46,7 +113,17 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
-
+function howManyTimesElementRepeated(wordsCount, key)
+{
+  count=0;
+  wordsCount.forEach(function(item){
+      if(key==item)
+      {
+        count++;
+      }
+  });
+  return count;
+}
 // Progression #8: Bonus
 
 const matrix = [
@@ -61,3 +138,26 @@ const matrix = [
   [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
   [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
 ];
+function maximumProduct(matrix)
+{
+        let arr = [];
+        let tsum=0,tsum1=0;
+        for(let i =0 ;i<2;i++)
+        {
+            for(let j=0;j<2;j++)
+            {
+                tsum=matrix[i][j]*matrix[i][j+1]*matrix[i][j+2]*matrix[i][j+3];
+                tsum1=matrix[i][j]*matrix[i+1][j]*matrix[i+2][j]*matrix[i+3][j];
+                if(tsum>tsum1)
+                {
+                    arr.push(tsum);
+                }
+                else{
+                    arr.push(tsum1);
+                }
+            }
+        }
+        arr.sort();
+        arr.reverse();
+        return arr;
+}
